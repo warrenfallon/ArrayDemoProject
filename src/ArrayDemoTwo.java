c92dcfff1e17affe11e6332d648400f478d7df7d
warrenfallon <warrenfallon@gmail.com>
2024-07-19 09:29:26 -0400

    Add ArrayDemoTwo example

---
+++ src/ArrayDemoTwo.java
@@ -0,0 +1,9 @@
+public class ArrayDemoTwo {
+    public static void main(String[] args) {
+        int[] age = {12, 4, 5};
+        System.out.println("Using for Loop:");
+        for (int j : age) {
+            System.out.println(j);
+        }
+    }
+}
