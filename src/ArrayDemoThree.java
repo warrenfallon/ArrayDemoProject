8c5c623d7f09b3d9ac16fb591b4ecb30472add36
warrenfallon <warrenfallon@gmail.com>
2024-07-19 10:17:09 -0400

    add ArrayDemoThree examole

---
+++ src/ArrayDemoThree.java
@@ -0,0 +1,17 @@
+public class ArrayDemoThree {
+    public static void main(String[] args) {
+        int[] numbers = {2, -9, 0, 5, 12, -25, 22, 9, 8, 12};
+        int sum = 0;
+        double average; // Change Double to double
+
+        for (int number : numbers) {
+            sum += number;
+        }
+
+        int arrayLength = numbers.length;
+        average = ((double) sum / (double) arrayLength);
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
+    }
+}
