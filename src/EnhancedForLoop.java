48f318145215a7679eec221cf0df969cd9f80161
warrenfallon <warrenfallon@gmail.com>
2024-07-19 10:04:41 -0400

    Add EnhancedForLoop example

---
+++ src/EnhancedForLoop.java
@@ -0,0 +1,8 @@
+public class EnhancedForLoop {
+    public static void main(String[] args) {
+        String[] names = { "Fallon", "Omar", "Raymond", "Kizzy" };
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
